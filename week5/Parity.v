a98a233d136e3f04cf4622568ad20957a84444d0
lota09 <88420999+lota09@users.noreply.github.com>
2024-04-03 22:53:36 +0900

    Update Parity.v

--- week5/Parity.v
+++ week5/Parity.v
@@ -4,3 +4,30 @@ module Parity(
 );
   assign y= ~(c||(a&b));
 endmodule
+
+`timescale 1ns/1ps
+module parity_tb;
+reg a,b,c;
+wire y;
+parity instance_parity(
+  .a(a),
+  .b(b),
+  .y(y)
+);
+
+inital begin
+a=0; b=0; c=0;
+#10; a=0; b=0; c=1;
+#10; a=0; b=1; c=0;
+#10; a=0; b=1; c=1;
+#10; a=1; b=0; c=0;
+#10; a=1; b=0; c=1;
+#10; a=1; b=1; c=0;
+#10; a=1; b=1; c=1;
+#10;
+$finish;
+end
+initial begin
+  $monitor("시간=%t, a=%b, b=%b, c=%b, y=%b", $time, a, b, c, y);
+end
+endmodule
