2529602a5e36eb0f1ba8484b4920d2f5fd36af46
lota09 <88420999+lota09@users.noreply.github.com>
2024-04-03 23:00:04 +0900

    Update Parity.v

--- week5/Parity.v
+++ week5/Parity.v
@@ -1,33 +1,37 @@
-module Parity(
-  input a,b,c,
-  output y
+module parity(
+    input a, b, c,
+    output y
 );
-  assign y= ~(c||(a&b));
+    assign y= ~(c||(a&b));
 endmodule
 
-`timescale 1ns/1ps
+`timescale 1ns/ 1ps
+
 module parity_tb;
 reg a,b,c;
 wire y;
+
 parity instance_parity(
-  .a(a),
-  .b(b),
-  .y(y)
+    .a(a),
+    .b(b),
+    .c(c),
+    .y(y)
 );
 
-inital begin
-a=0; b=0; c=0;
-#10; a=0; b=0; c=1;
-#10; a=0; b=1; c=0;
-#10; a=0; b=1; c=1;
-#10; a=1; b=0; c=0;
-#10; a=1; b=0; c=1;
-#10; a=1; b=1; c=0;
-#10; a=1; b=1; c=1;
-#10;
-$finish;
-end
 initial begin
-  $monitor("시간=%t, a=%b, b=%b, c=%b, y=%b", $time, a, b, c, y);
+    a=0; b=0; c=0; #10;
+    a=0; b=0; c=1; #10;
+    a=0; b=1; c=0; #10;
+    a=0; b=1; c=1; #10;
+    a=1; b=0; c=0; #10;
+    a=1; b=0; c=1; #10;
+    a=1; b=1; c=0; #10;
+    a=1; b=1; c=1; #10;
+    $finish;
 end
+
+initial begin
+    $monitor("시간=%t, a=%b, b=%b, c=%b, y=%b", $time, a, b, c, y);
+end
+
 endmodule
